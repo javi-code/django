85f9189088ff165bb40ee508777e07db6dc72caa
Javier Avila <javier.avila.sj@gmail.com>
2022-11-17 09:38:01 -0400

    add staticfiles

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import CategoryShop, Product, Shop, User, CategoryProduct, Image
+
+class UserAdmin(admin.ModelAdmin):
+   list_display = ('username', 'name', 'email', 'phone','role')
+   search_fields = ('username', 'name', 'email', 'phone')
+
+class ProductAdmin(admin.ModelAdmin):
+   list_display = ('name', 'shop', 'category')
+   list_filter = ['category']
+   search_fields = ['name']
+
+admin.site.register(Image)
+# admin.site.register(User, UserAdmin)
+admin.site.register(Shop)
+admin.site.register(CategoryShop)
+admin.site.register(CategoryProduct)
+admin.site.register(Product, ProductAdmin)
